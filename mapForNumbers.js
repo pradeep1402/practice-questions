031600c4d8a05608c2af6d8408abf68975b57bf9
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2024-12-22 14:03:57 +0530

    cumulativeSumsOf

--- mapForNumbers.js
+++ mapForNumbers.js
@@ -18,4 +18,23 @@ const reversedArraysOf = function (arrays) {
   return arrays.map(function (numbers) { return numbers.reverse(); });
 };
 
-console.log(reversedArraysOf([[1, 2, 3], [4, 5, 6]]));
+// console.log(reversedArraysOf([[1, 2, 3], [4, 5, 6]]));
+
+// cumulative sums of [[1, 2, 3], [4, 5, 6]] => [[1, 3, 6], [4, 9, 15]]
+// Example: cumulative sum of [1, 2, 3] is [1, 1+2, 1+2+3]
+const runningSum = function (numbers) {
+  const cumulative = [];
+  let sum = 0;
+
+  numbers.map(function (number) {
+    sum += number;
+    cumulative.push(sum);
+  });
+
+  return cumulative;
+};
+const cumulativeSumsOf = function (arrays) {
+  return arrays.map(runningSum);
+};
+
+console.log(cumulativeSumsOf([[1, 2, 3], [4, 5, 6]]));
