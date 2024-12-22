f3ce1f0ad4c87da786b1323258ab99dbb4907999
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2024-12-22 15:38:33 +0530

    firstLettersOfNames

--- mapOnArrayOfObject.js
+++ mapOnArrayOfObject.js
@@ -10,4 +10,11 @@ const extractAges = function (objects) {
   return objects.map(function (person) { return person.age; });
 };
 
-console.log(extractAges([{ age: 25 }, { age: 30 }]));
+// console.log(extractAges([{ age: 25 }, { age: 30 }]));
+
+// extract the first letters of names from [{ name: "Alice" }, { name: "Bob" }] => ["A", "B"]
+const firstLettersOfNames = function (objects) {
+  return objects.map(function (person) { return person.name.at(0); });
+};
+
+console.log(firstLettersOfNames([{ name: "Alice" }, { name: "Bob" }]));
