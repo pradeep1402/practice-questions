24799e3b94c147de303dd567c33ed824ebc51d02
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2024-12-22 11:11:57 +0530

    lengthsOf

--- mapSimpleOperation.js
+++ mapSimpleOperation.js
@@ -3,4 +3,11 @@ const squaresOf = function (numbers) {
   return numbers.map(function (number) { return number * number; });
 };
 
-console.log(squaresOf([1, 2, 3]));
+// console.log(squaresOf([1, 2, 3]));
+
+// lengths of ["apple", "banana", "kiwi"] => [5, 6, 4]
+const lengthsOf = function (strings) {
+  return strings.map(function (word) { return word.length; });
+};
+
+console.log(lengthsOf(["apple", "banana", "kiwi"]));
