07c06934c79e9412e34d5cf278934fdae9d2bc0e
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2024-12-22 22:50:09 +0530

    filterNumbersGreaterThanTen

--- filterOnNumber.js
+++ filterOnNumber.js
@@ -7,4 +7,11 @@ const filterEvenNumbers = function (numbers) {
   return numbers.filter(isEven);
 };
 
-console.log(filterEvenNumbers([1, 2, 3, 4, 5]));
+// console.log(filterEvenNumbers([1, 2, 3, 4, 5]));
+
+// numbers greater than 10 [5, 12, 7, 18, 3] => [12, 18]
+const filterNumbersGreaterThanTen = function (numbers) {
+  return numbers.filter(function (number) { return number > 10; });
+};
+
+console.log(filterNumbersGreaterThanTen([5, 12, 7, 18, 3]));
