a203a84de2a0246983f610395baef57b2aa3b890
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2024-12-22 13:47:17 +0530

    reversedArraysOf

--- mapForNumbers.js
+++ mapForNumbers.js
@@ -13,26 +13,6 @@ const charCodesOf = function (strings) {
 
 // console.log(charCodesOf(["a", "b", "c"]));
 
-// count vowels in ["apple", "banana", "grape"] => [2, 3, 2]
-const countVowel = function (letters) {
-  const vowels = ['a', 'e', 'i', 'o', 'u'];
-  let count = 0;
-
-  for (const letter of letters) {
-    if (vowels.includes(letter)) {
-      count++;
-    }
-  }
-
-  return count;
-};
-
-const countVowelsOf = function (strings) {
-  return strings.map(function (word) { return countVowel([...word]); });
-};
-
-// console.log(countVowelsOf(["apple", "banana", "grape"]));
-
 // reverse arrays of [[1, 2, 3], [4, 5, 6]] => [[3, 2, 1], [6, 5, 4]]
 const reversedArraysOf = function (arrays) {
   return arrays.map(function (numbers) { return numbers.reverse(); });
