2496f824ea9a368085b0b9431e94b365cfc5849d
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2024-12-22 13:11:27 +0530

    charCodesOf

--- mapForNumbers.js
+++ mapForNumbers.js
@@ -4,3 +4,11 @@ const squaresOf = function (numbers) {
 };
 
 // console.log(squaresOf([1, 2, 3]));
+
+// character codes of ["a", "b", "c"] => [97, 98, 99]
+// Use the `charCodeAt` method on each string
+const charCodesOf = function (strings) {
+  return strings.map(function (char) { return char.charCodeAt(); });
+};
+
+console.log(charCodesOf(["a", "b", "c"]));
