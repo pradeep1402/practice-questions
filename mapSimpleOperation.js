cf76f8854e5572828c9b304e0007b90c4fcf0956
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2024-12-22 11:13:34 +0530

    uppercaseOf

--- mapSimpleOperation.js
+++ mapSimpleOperation.js
@@ -10,4 +10,11 @@ const lengthsOf = function (strings) {
   return strings.map(function (word) { return word.length; });
 };
 
-console.log(lengthsOf(["apple", "banana", "kiwi"]));
+// console.log(lengthsOf(["apple", "banana", "kiwi"]));
+
+// uppercase of ["hello", "world"] => ["HELLO", "WORLD"]
+const uppercaseOf = function (strings) {
+  return strings.map(function (word) { return word.toUpperCase(); });
+};
+
+console.log(uppercaseOf(["hello", "world"]));
