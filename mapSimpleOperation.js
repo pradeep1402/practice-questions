a1a6a5a8cdabe3760e8c6060af2ff5172735869d
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2024-12-22 11:16:01 +0530

    firstCharactersOf

--- mapSimpleOperation.js
+++ mapSimpleOperation.js
@@ -17,4 +17,11 @@ const uppercaseOf = function (strings) {
   return strings.map(function (word) { return word.toUpperCase(); });
 };
 
-console.log(uppercaseOf(["hello", "world"]));
+// console.log(uppercaseOf(["hello", "world"]));
+
+// first characters of ["apple", "banana", "kiwi"] => ["a", "b", "k"]
+const firstCharactersOf = function (strings) {
+  return strings.map(function (word) { return word.at(0); });
+};
+
+console.log(firstCharactersOf(["apple", "banana", "kiwi"]));
