764bf6f2f9a3002c8334fa483f9a1215ea1299b0
Pradeep Bisht <107491133+pradeep1402@users.noreply.github.com>
2024-12-26 20:54:10 +0530

    initial commit

---
+++ reduce/reduceOnNumbers.js
@@ -0,0 +1,41 @@
+// sumOf([1, 2, 3, 4]) => 10
+const sumOf = function (numbers) { };
+
+// productOf([1, 2, 3, 4]) => 24
+const productOf = function (numbers) { };
+
+// averageOf([1, 2, 3, 4, 5]) => 3
+const averageOf = function (numbers) { };
+
+// minOf([3, 1, 4, 1, 5, 9, 2]) => 1
+const minOf = function (numbers) { };
+
+// maxOf([3, 1, 4, 1, 5, 9, 2]) => 9
+const maxOf = function (numbers) { };
+
+// sumPositiveNumbers([1, -2, 3, -4]) => 4
+const sumPositiveNumbers = function (numbers) { };
+
+// sumOfSquares([1, 2, 3, 4]) => 30
+const sumOfSquares = function (numbers) { };
+
+// sumOfOddNumbers([1, 2, 3, 4, 5]) => 9
+const sumOfOddNumbers = function (numbers) { };
+
+// countNegativeNumbers([1, -2, 3, -4]) => 2
+const countNegativeNumbers = function (numbers) { };
+
+// findSumOfEvenSquares([1, 2, 3, 4]) => 20
+const findSumOfEvenSquares = function (numbers) { };
+
+// duplicateNumbers([1, 2, 3]) => [1, 1, 2, 2, 3, 3]
+const duplicateNumbers = function (numbers) { }
+
+// concatenateArrays([[1, 2], [3, 4], [5, 6]]) => [1, 2, 3, 4, 5, 6]
+const concatenateArrays = function (arrays) { }
+
+// flattenArray([[1, 2], [3, 4], [5, 6]]) => [1, 2, 3, 4, 5, 6]
+const flattenArray = function (arrays) { }
+
+// uniqueNumbers([1, 2, 2, 3, 4, 4, 5]) => [1, 2, 3, 4, 5]
+const uniqueNumbers = function (numbers) { }
---
+++ reduce/reduceOnString.js
@@ -0,0 +1,30 @@
+
+// concatenateWords(["hello", "world"]) => "helloworld"
+const concatenateWords = function (words) { };
+
+// longestWord(["apple", "banana", "cherry", "kiwi"]) => "banana"
+const longestWord = function (words) { };
+
+// shortestWord(["apple", "banana", "cherry", "kiwi"]) => "kiwi"
+const shortestWord = function (words) { };
+
+// joinWithComma(["apple", "banana", "cherry"]) => "apple,banana,cherry"
+const joinWithComma = function (words) { };
+
+// reverseWords(["hello", "world"]) => "world hello"
+const reverseWords = function (words) { };
+
+// joinWordsWithSpace(["apple", "banana", "cherry"]) => "apple banana cherry"
+const joinWordsWithSpace = function (words) { };
+
+// concatenateNames(["John", "Jane", "Doe"]) => "JohnJaneDoe"
+const concatenateNames = function (names) { };
+
+// countVowelsInWords(["hello", "world"]) => "eoo"
+const countVowelsInWords = function (words) { };
+
+// makeCamelCase(["hello", "world", "how", "are", "you"]) => "helloWorldHowAreYou"
+const makeCamelCase = function (words) { };
+
+// reverseString(["apple", "banana", "cherry"]) => "elppaananabyrrehc"
+const reverseString = function (words) { };
